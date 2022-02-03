e78ab6da6eddf9efba419a42c0b954da15350ca0
BiliciMiodrag <58682940+BiliciMiodrag@users.noreply.github.com>
2022-02-03 23:24:23 +0200

    Engine
    Engine

---
+++ CustommerEngineServiceInstaller.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace Manager
+
+{
+    [RunInstaller(true)]
+    public class CustommerEngineServiceInstaller : Installer
+    {
+        private ServiceProcessInstaller process;
+        private ServiceInstaller service;
+        public CustommerEngineServiceInstaller()
+        {
+            process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.LocalSystem;
+            service = new ServiceInstaller();
+            service.ServiceName = "CustommerEngineService";
+            Installers.Add(process);
+            Installers.Add(service);
+        }
+    }
+}
